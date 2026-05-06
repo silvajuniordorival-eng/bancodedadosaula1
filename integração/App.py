49252b79e667c4e8ac83e0673db910eef7494fd5
silvajuniordorival-eng <silva.junior.dorival@escola.pr.gov.br>
2026-05-06 19:23:03 -0300

    Add files via upload

---
+++ integração/App.py
@@ -0,0 +1,66 @@
+#import do flask para criação do servidor
+#reder_template para criar uma "ponte" com html
+#request para capturar os dados digitados
+from flask import Flask,render_template,request
+import mysql.connector
+
+#"ajuda" o Flask a localizar os caminhos dos arquivos
+app = Flask(__name__)
+
+db_config = {
+    'host': 'localhost',
+    'user': 'root',
+    'password': 'escola',
+    'database': 'cadastro'
+}
+
+#criando a rota para acessar o arquivo html
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/')
+def index():
+    try:
+        #
+        conectar = mysql.connector.connect(**db_config)
+        cursor = conectar.cursor(dictionary=True)
+
+        #seleção de tabela
+        cursor.execute("SELECT CPF,PRIMEIRO_NOME,SOBRENOME,IDADE FROM cliente")
+        lista_clientes = cursor.fetchall()
+
+        cursor.close()
+        conectar.close()
+        return render_template('index.html', clientes = lista_clientes)
+    
+    except mysql.connector.Error as erro:
+        return f"Erro ao carregar a Tabela: {erro}"
+
+#criem uma rota para acessar o formulario
+@app.route('/cadastrar', methods=['POST'])
+def form_cadastro():
+    #bloco para armazenar os dados digitados
+    CPF = request.form['cpf']
+    primeiro_nome = request.form['primeiro_nome']
+    sobrenome = request.form['sobrenome']
+    idade = request.form['idade']
+
+    try:
+        #verificando conexão com Mysql
+        conectar = mysql.connector.connect(**db_config)
+        cursor = conectar.cursor()
+        query = "INSERT INTO cliente(CPF,PRIMEIRO_NOME,SOBRENOME,IDADE) VALUES (%s,%s,%s,%s)"
+        cursor.execute(query,(CPF,primeiro_nome,sobrenome,idade))
+
+        #atualiza as alterações e fecha as conexões
+        conectar.commit()
+        cursor.close()
+        conectar.close()
+
+        return f"<h3> Cliente {primeiro_nome} salvo com sucesso!</h3> <a href='/'>Voltar</a>"
+    except mysql.connector.Error as erro:
+        return f"Erro ao gravar no banco: {erro}"
+
+if __name__ == '__main__':
+    app.run(debug=True) 
